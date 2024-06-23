85cad9eeb4c9988c0c0460ea3da25fe596f5d671
unknown <zaratelorenza@gmail.com>
2024-06-23 20:43:47 -0300

    archivo app.py actualizado hasta etapa 3

--- app.py
+++ app.py
@@ -1,179 +1,116 @@
-# Pasamos de funciones a OBJETOS
-# ----------------------------------------------------------------------------------------
-# Definimos una clase
-#-----------------------------------------------------------------------------------------
+# Etapa 3
+import mysql.connector
 
 class Catalogo:
 
-    #Definimos una lista para almacenar los paquetes turísticos.
-    #Es una lista de paquetes turísticos.
-    destinos = []
-    #------------------------------------------------------------------------------------
-    # Método para agregar un paquete turístico.
-    #------------------------------------------------------------------------------------
+    def __init__(self, host, user, password, database):
 
-    def agregarPaqTuris(self,codigo,descripcion,cantidad, precio, imagen,estadia,fecha):
+        self.conn = mysql.connector.connect(
+          host=host,
+          user=user,
+          password=password,
+          database=database
+        )
+        self.cursor = self.conn.cursor(dictionary=True)
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS destinos (
+            codigo INT AUTO_INCREMENT PRIMARY KEY,
+            descripcion VARCHAR(255) NOT NULL,
+            cantidad INT NOT NULL,
+            precio DECIMAL(10, 2) NOT NULL,
+            imagen_url VARCHAR(255), 
+            estadia VARCHAR (255)
+            fecha VARCHAR (255))''')
 
-         """
-         Parámetros:
-         -código: int, código numérico del paquete turístico.
-         -descripción: str, descripción del destino.
-         -cantidad:int,cantidad de paquetes turísticos disponibles.
-         -precio: float, precio de venta.
-         -imagen: str, nombre de la imagen(destino).
-         -estadía: str, duración de estadía.
-         -fecha : str, fechas de ida y vuelta.
-          Retorna:
-         -bool: True si se agregó el paquete t.,False si ya existe un paquete t. con el mismo código.
-         """
-        #Verificamos si ya existe un paquete/destino con el mismo código
+        self.conn.commit()
 
-         if self.consultarPaqTuris(codigo):
-           print ("Paquete turístico existente.")
-           return False # ya existe un destino con el mismo código.
-                    
-         else:
-         #Creamos un diccionario con los datos del paquete t.
-            nuevoPaqTuris = {
-            "codigo": codigo,
-            "descripcion": descripcion,
-            "cantidad":cantidad,
-            "precio": precio,
-            "imagen": imagen,
-            "estadia": estadia,
-            "fecha": fecha
-                    }
-                    
-         #Agregamos el paquete turístico al array (destinos)
-            self.destinos.append(nuevoPaqTuris)
-            return True #El paquete se agregó exitosamente.
+    """Agregamos un nuevo paquete turístico a la base de datos, para ello necesitamos
+      los parámetros que describen las características del paquete que agregamos."""
+    def agregarPaqTuris(self, descripcion, cantidad, precio, imagen, estadia, fecha):
+      sql = "INSERT INTO destinos (descripcion, cantidad, precio, imagen_url, estadia, fecha)VALUES (%s, %s, %s, %s, %s,%s)" 
+      valores = (descripcion, cantidad, precio, imagen,estadia,fecha)
+      self.cursor.execute(sql,valores) 
+      self.conn.commit()
+      return self.cursor.lastrowid
+    
+    # Consultamos un paquete t. a partir de su código.
+    def consultarPaqTuris(self, codigo):
+       self.cursor.execute(f"SELECT * FROM productos WHERE codigo = {codigo}")
+       return self.cursor.fetchone()
+    
+    # Actualizamos los datos de un paquete específico en la base de datos a partir de su código.
+    def modificarPaqTuris(self, codigo, nuevaDescripcion, nuevaCantidad, 
+    nuevoPrecio, nuevaImagen, nuevaEstadia, nuevaFecha):
+      sql = "UPDATE destinos SET descripcion = %s, cantidad = %s, precio = %s, imagen_url = %s, estadia = %s, fecha = %s WHERE codigo = %s"
+      valores = (nuevaDescripcion, nuevaCantidad, nuevoPrecio, nuevaImagen, nuevaEstadia, nuevaFecha, codigo)
+      self.cursor.execute(sql, valores)
+      self.conn.commit()
+      return self.cursor.rowcount > 0
+    
+    #Mostramos los datos de un paquete a partir de su código.
+    def mostrarPaqTuris(self, codigo): 
+        paqTuris = self.consultarPaqTuris(codigo)
+        if paqTuris:
+            print("-" * 40)
+            print(f"Código.....: {paqTuris['codigo']}")
+            print(f"Descripción: {paqTuris['descripcion']}")
+            print(f"Cantidad...: {paqTuris['cantidad']}")
+            print(f"Precio.....: {paqTuris['precio']}")
+            print(f"Imagen.....: {paqTuris['imagen_url']}")
+            print(f"Estadía....: {paqTuris['estadia']}")
+            print(f"Fecha......: {paqTuris['fecha']}")
+            print("-" * 40)
+        else:
+            print("Producto no encontrado.")
 
-    #------------------------------------------------------------------------------------
-    # Método para consultar si un paquete existe en el arreglo, a partir de su código
-    #------------------------------------------------------------------------------------
+    #Mostramos todos los paquetes turísticos de la base de datos.
+    def listarDestinos(self):
+         self.cursor.execute("SELECT * FROM destinos")
+         destinos = self.cursor.fetchall()
+         return destinos
+    
+    # Eliminamos un paquete de la tabla a partir de su código.
+    def eliminarPaqTuris(self, codigo):
+        self.cursor.execute(f"DELETE FROM destinos WHERE codigo = {codigo}")
+        self.conn.commit()
+        return self.cursor.rowcount > 0
+    
+    
+ # Programa principal
 
-    def consultarPaqTuris(self,codigo):
-       """
-        Consulta un paquete/destino a partir de su código y devuelve sus datos.
-        Parámetros:
-        -codigo: int, código numérico del producto.
-        Retorna:
-        -dict: datos del destino en forma del diccionario,o False si no se encontró el destino.
-        """
-         #Recorremos la lista de destinos.
+catalogo = Catalogo(host='localhost', user='root', password='', database='viajes')
 
-       for paqTuris in self.destinos:
-                            #Si el código existe
-            if paqTuris["codigo"] == codigo:
-               return paqTuris
-                    #Si el bucle finaliza sin encontrar el paquete turístico
-       return False
-                        
-    #------------------------------------------------------------------------------------
-    # Método para modificar datos de un paquete, a partir de su código
-    #------------------------------------------------------------------------------------
-    def modificarPaqTuris(self,codigo,nuevaDescripcion,nuevaCantidad,nuevoPrecio,
-                                            nuevaImagen,nuevaEstadia,nuevaFecha):
-                            
-        """
-          Modifica los datos de un destino a partir de su código:
-          Parámetros:
-          -código: int, código numérico del paquete turístico.
-          -nuevaDescripcion: str, nueva descripción del destino.
-          -nuevaCantidad:int, nueva cantidad de paquetes turísticos.
-          -nuevoPrecio: float,nuevo precio de venta.
-          -nuevaImagen: str,nueva imagen del paquete(destino).
-          -nuevaEstadia: str, nueva duración de estadía.
-          -nuevaFecha : str,nueva fecha (de ida y vuelta).
-        """           
-         #Recorremos la lista de destinos...
-        for paqTuris in self.destinos:
-            if paqTuris["codigo"] == codigo:
-                 paqTuris["descripcion"] = nuevaDescripcion
-                 paqTuris["cantidad"] = nuevaCantidad
-                 paqTuris["precio"] = nuevoPrecio
-                 paqTuris["imagen"] = nuevaImagen
-                 paqTuris["estadia"] = nuevaEstadia
-                 paqTuris["fecha"] = nuevaFecha
+# Agregamos paquetes turísticos a la tabla destinos
+"""catalogo.agregarPaqTuris(1,"Córdoba,Argentina",40,450000,"Córdoba.png","5 noches","15/07/24 al 19/07/24")
+   catalogo.agregarPaqTuris(2,"Mendoza,Argentina",30, 500000, "Mendoza.png","7 noches","15/07/24 al 21/07/24")
+   catalogo.agregarPaqTuris(3,"Buenos Aires,Argentina",50, 450000, "Buenos Aires.png","5 noches","15/07/24 al 19/07/24")
+   catalogo.agregarPaqTuris(4,"Bariloche,Argentina",60, 800000, "Bariloche.png","7 noches","15/07/24 al 21/07/24")
+   catalogo.agregarPaqTuris(5,"Cataratas del Iguazú,Argentina",60,300000, "Cataratas del Iguazú.png","3 noches","15/07/24 al 17/07/24")"""
 
-                 return True
-                    #Si llegamos hasta aquí, el paquete turístico no existe
-        return False
-    #------------------------------------------------------------------------------------------
-    # Método para obtener un listado de los paquetes en pantalla
-    #------------------------------------------------------------------------------------------
+# Consultamos un paquete y lo mostramos
+"""codPaqTuris = int(input("Ingrese el código del paquete turístico: "))
+   paqTuris = catalogo.consultarPaqTuris(codPaqTuris)
+    if paqTuris:
+           print(f"Paquete turístico encontrado: {paqTuris['codigo']} - {paqTuris['descripcion']}")
+    else:
+          print(f'Paquete turístico {codPaqTuris} no encontrado.')"""
 
-    def listarPaqTuris(self):
-                    
-    # Muestra en pantalla un listado de los destinos existentes.Recorremos la lista. 
-                    
-        print("-"*50)
-        for paqTuris in self.destinos:
-           #Mostramos los datos de cada destino
-            print(f"Código..........:    {paqTuris["codigo"]}")
-            print(f"Descripción.....:    {paqTuris["descripcion"]}")
-            print(f"Cantidad........:    {paqTuris["cantidad"]}")
-            print(f"Precio..........:    {paqTuris["precio"]}")
-            print(f"Imagen..........:    {paqTuris["imagen"]}")
-            print(f"Estadía.........:    {paqTuris["estadia"]}")
-            print(f"Fecha...........:    {paqTuris["fecha"]}")
-            print("-"*50)
+#Modificamos un paquete y lo mostramos
+"""catalogo.mostrarPaqTuris(1)
+   catalogo.modificarPaqTuris(1,"Córdoba,Argentina",40,5000000,"Córdoba.png","6 noches","15/07/24 al 20/07/24") # modificamos precio, estadia y fecha
+   catalogo.mostrarPaqTuris(1)"""
 
-    #--------------------------------------------------------------------------------------------
-    # Método para eliminar un paquete, a partir de su código.
-    #--------------------------------------------------------------------------------------------
-    def eliminarPaqTuris(self,codigo):
-     """
-     Elimina un paquete a partir de su código:
-     Parámetro:
-     -código: int, código numérico del paquete turístico.
-     """
-         #Recorremos la lista de destinos/paquetes
+#Listar paquetes turísticos (Todos)
+#destinos = catalogo.listarDestinos()
+#for paqTuris in destinos:
+      #print (paqTuris)
 
-     for paqTuris in self.destinos:
-         if paqTuris["codigo"] == codigo:
-            print (f"Paquete turístico {paqTuris["descripcion"]} eliminado.")
-            self.destinos.remove(paqTuris)
-                        
-            return True
-        #Si llegamos hasta aquí, el paquete no existe
-     return False
-                    
-""">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# Eliminamos un paquete
+#catalogo.eliminarPaqTuris(2)
+#destinos = catalogo.listarDestinos()
+#for paqTuris in destinos:
+    #print (paqTuris)
+#catalogo.listarDestinos()
 
-                            EJEMPLOS DE USO DE MÉTODOS
-
->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"""
-
-"""catalogo = Catalogo()
-
-                    
-            # Agregamos paquetes turísticos a la lista
-catalogo.agregarPaqTuris(1,"Salta,Argentina",40,450000,"Salta.png","5 noches","15/07/24 al 19/07/24")
-catalogo.agregarPaqTuris(2,"San Rafael,Argentina",30, 500000, "San Rafael.png","7 noches","15/07/24 al 21/07/24")
-catalogo.agregarPaqTuris(3,"Mar del Plata,Argentina",50, 450000, "Mar del Plata.png","5 noches","15/07/24 al 19/07/24")
-catalogo.agregarPaqTuris(4,"Bariloche,Argentina",60, 800000, "Bariloche.png","7 noches","15/07/24 al 21/07/24")
-catalogo.agregarPaqTuris(5,"Cataratas del Iguazú,Argentina",60,300000, "Cataratas del Iguazú.png","3 noches","15/07/24 al 17/07/24")
-            
-            #Consulta de paquetes turísticos
-                
-paqTuris = catalogo.consultarPaqTuris(1)
-if paqTuris:
-                    print(f"Paquete turístico encontrado:{paqTuris["descripcion"]}")
-else:
-                    print("Paquete turístico no encontrado.")
-
-            #Modificación de un destino/paquete                 
-catalogo.modificarPaqTuris(1,"Salta,Argentina",50,460000,"Salta.png","5 noches","15/07/24 al 19/07/24")
-
-            #Listado de paquetes turísticos
-catalogo.listarPaqTuris()
-
-            #Eliminación de paquete turístico
-print()
-catalogo.eliminarPaqTuris(1)
-print()
-catalogo.listarPaqTuris()"""
 
 
 
